7e0b8dcc015447ee8d5eeb04cfa5906f3428998d
João Cláudio <joaoclaudiobio@gmail.com>
2023-05-25 15:55:15 -0300

    testando código

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,17 @@
-// Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const apiRain = fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`);
+  console.log(apiRain);
+  apiRain.then(
+    (response) => {
+      console.log(response);
+      const resultPromise = response.json();
+      resultPromise.then((data) => {
+        console.log(data);
+      });
+    },
+  );
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
